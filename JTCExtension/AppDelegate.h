125340d12ff2dce1a4ec109416a1c0e31cf4bb61
TimZ <huai_1989@yeah.net>
2015-12-23 15:48:33 +0800

    init

---
+++ JTCExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JTCExtension
+//
+//  Created by iMac on 15/12/23.
+//  Copyright © 2015年 TZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
